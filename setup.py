0453bf2f10584683861480140ca36702b55dbdbc
David Wade <dawad@equinor.com>
2020-03-25 13:24:51 +0000

    Temporary package name

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, Extension, Command
 import numpy as np
 
 setup(
-    name="zfpy",
+    name="zfpy-equinor",
     version="0.5.5",
     author="Peter Lindstrom",
     author_email="zfp@llnl.gov",
     description="zfp compression in Python",
     long_description="zfp compression in Python",
-    ext_modules = [Extension('zfpy', ['build/python/zfpy.c'], 
+    ext_modules = [Extension('zfpy-equinor', ['build/python/zfpy.c'], 
                              include_dirs=['include', np.get_include()], 
                              libraries=['zfp'], library_dirs=['build/lib64'])]
 )
